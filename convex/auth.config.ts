b2502aa97c6ee03948b9173c70ee2651e2122c64
davidwyh-dev <your_email@example.com>
2026-01-22 17:42:06 -0500

    Changes to env variable usage

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,10 @@
+// The SITE_URL environment variable must be set in each Convex deployment:
+// Dev:  npx convex env set SITE_URL https://pastel-clownfish-426.convex.site
+// Prod: npx convex env set SITE_URL https://efficient-hound-679.convex.site --prod
 export default {
   providers: [
     {
-      domain: "https://pastel-clownfish-426.convex.site",
+      domain: process.env.SITE_URL,
       applicationID: "convex",
     },
   ],
